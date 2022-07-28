845dff5567dcc6971c24272f011c3fb0d04c7cff
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-28 01:35:10 +0100

    'updated'

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,12 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// alert('Open the console and check if the countries has been loaded')
+
+const numbers = [1, 2, 3, 4, 5]
+const newArr = []
+let sum = 0
+for(let i = 0; i < numbers.length; i++){
+  newArr.push( numbers[i] ** 2)
+
+}
+
+console.log(newArr) 
