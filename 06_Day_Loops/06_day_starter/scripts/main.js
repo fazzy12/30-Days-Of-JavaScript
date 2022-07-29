6acc24cdd9d5793e942122bb3ce0395abb511514
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-29 14:13:20 +0100

    updated file

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,12 +1,278 @@
-console.log(countries)
-// alert('Open the console and check if the countries has been loaded')
 
-const numbers = [1, 2, 3, 4, 5]
-const newArr = []
-let sum = 0
-for(let i = 0; i < numbers.length; i++){
-  newArr.push( numbers[i] ** 2)
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
 
-}
+//   const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB'
+//   ]
 
-console.log(newArr) 
+
+//   const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+//   // number 1
+
+// //   for loop
+//   for (let i = 0; i <= 10; i++){
+//     console.table(i)
+//   }
+
+// // d0 while loop
+// let num = 0
+// do {
+//     console.table(num)
+//     num++
+// //     
+// }while (num <= 10)
+
+// // while loop
+// let nums = 0
+
+// while (nums <= 10){
+//     console.table(nums)
+//     nums = nums + 1
+// }
+
+// // numbers 2
+// for (let i = 10; i >= 0; --i){
+//     console.table(i)
+//   }
+
+// // d0 while loop
+// let numdec = 10
+// do {
+//     console.log(numdec)
+//     --numdec
+   
+// }while (numdec >= 0)
+
+// // while loop
+// let nums1 = 10
+
+// while (nums1 >= 0){
+//     console.table(nums1)
+//     nums1--
+// }
+
+// // number 3
+
+// // number 4
+
+// let string = ""
+// for (let i = 1; i < 7; i++) {
+//     for (let j = 0; j < i; j++) {
+//         string += '#'
+//     }
+//     string += "\n";
+// }
+
+// console.log(string)
+
+// // number 5
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${i} * ${i} = ${i * i}`)
+// }
+
+// number 6
+// const io = 'i'
+// const ii = 'i^2'
+// const iii = 'i^3'
+// console.log(`${io} ${ii} ${iii}`)
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${i}`  `${i * i}`  `${i * i * i}`)
+// }
+
+// numbers 7
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2){
+//     console.log(`${i}`)
+//   } else{
+//     console.log(``)
+//   }
+// }
+
+
+// numbers 8
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 3){
+//     console.log(`${i}`)
+//   } else{
+//     console.log(``)
+//   }
+// }
+
+// numbers 9
+// let flag = 0
+// let num = 100
+// for (let i = 0;  i <= num; i++) {
+
+//   if (num % 2 == 0) {
+//     flag = 1;
+//     break
+//   }
+  
+  
+// }
+
+// if (flag == 0) {
+//   console.log(`${num} is a prime number`)
+// }else{
+//   console.log(` ${num} is not a prime number`)
+// }
+
+//  number 10
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//   sum = sum + i;
+  
+// }
+// console.log(` ${sum} `)
+
+// number 11
+// let sumEven = 0
+// let sumOdd = 0
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 == 0){
+//     sumEven = sumEven + i
+//   }else {
+//     sumOdd = sumOdd + i
+//   }
+
+// }
+// console.log(` ${sumEven} ${sumOdd} `)
+
+// number 12
+// let sumEven = 0
+// let sumOdd = 0
+// let arr = []
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 == 0){
+//     sumEven = sumEven + i
+//   }else {
+//     sumOdd = sumOdd + i
+//   }
+
+
+
+// }
+// arr.push(sumEven)
+// arr.push(sumOdd)
+// console.log(arr);
+
+// number 13
+// let randNum = '1234567890'
+// let charlen = randNum.length
+// let result = []
+
+// for (let i = 0; i < 5; i++) {
+//   const nums = randNum.charAt(Math.floor(Math.random() * charlen))
+//   result.push(nums)
+// }
+// console.log(result)
+
+// number 14
+
+// let randNum = '1234567890abcdefghijklmnopqrstuvwxyz'
+// let charlen = randNum.length
+// let result = ''
+
+// for (let i = 0; i < 6; i++) {
+//   result += randNum.charAt(Math.floor(Math.random() * charlen))
+// }
+// console.log(result)
+
+// level 2
+
+// number 1
+
+// let randNum = '1234567890abcdefghijklmnopqrstuvwxyz'
+// let charlen = randNum.length
+// let result = ''
+
+// for (let i = 0; i < charlen; i++) {
+//   result += randNum.charAt(Math.floor(Math.random() * charlen))
+// }
+// console.log(result)
+
+// number 2
+// let randNum = '1234567890abcdef'
+// let charlen = randNum.length
+// let result = ''
+
+// for (let i = 0; i < 6; i++) {
+//   result += randNum.charAt(Math.floor(Math.random() * charlen))
+// }
+// console.log(`#${result}`)
+
+// number 3
+  // var num = Math.round(0xffffff * Math.random());
+  // var r = num >> 16;
+  // var g = num >> 8 & 255;
+  // var b = num & 255;
+  // let rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  // console.log(rgb);
+
+// number 4
+ 
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+  ]
+//   console.log(countries)
+//   countries.map(function(country) {
+//     country.toUpperCase()
+//   })
+//   console.log(country)
+
+// number 6
+
+
+
+
+  // const webTechs = [
+  //   'HTML',
+  //   'CSS',
+  //   'JavaScript',
+  //   'React',
+  //   'Redux',
+  //   'Node',
+  //   'MongoDB'
+  // ]
+
+
+  // number 6
+
+  // level 3
+
+  // number 1
+  
+
+
+
