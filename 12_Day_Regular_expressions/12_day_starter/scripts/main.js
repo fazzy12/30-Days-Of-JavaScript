d7623388ad33e62a71119c5bb67721d309bf2ac7
fazzy12 <kaluifeanyi00@gmail.com>
2022-08-09 13:47:53 +0100

    update file

--- 12_Day_Regular_expressions/12_day_starter/scripts/main.js
+++ 12_Day_Regular_expressions/12_day_starter/scripts/main.js
@@ -1,2 +1,13 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+// const str =  'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
+
+// const reg = /\d{4,5}/g
+
+// const match = str.match(reg)
+
+// const sum = match.reduce((acc, curr) => parseInt(acc) + parseInt(curr), 0)
+// console.log(sum)
+
+const reg = /\d{1,2}/g
+console.log(reg)
+
